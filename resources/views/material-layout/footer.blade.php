019ad90ad35219e6bc48e769b0f35869dc1decf3
Don Bernardo <aedenbernardo@ymail.com>
2017-02-28 09:06:28 +0800

    Update footer.blade.php

--- resources/views/material-layout/footer.blade.php
+++ resources/views/material-layout/footer.blade.php
@@ -5,7 +5,7 @@
             <script>
                 document.write(new Date().getFullYear())
             </script>
-            <a href="http://nspirenetwork.com">Nspire Network LLC.</a>, Mission Driven People
+            <a href="http://nspirenetwork.com">Nspire Network LLC</a> &copy; 2017 All Rights Reserved.
         </p>
     </div>
 </footer>
